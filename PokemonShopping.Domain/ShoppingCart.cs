8235dcc84b4bb84efcc54d7cd202c101ee6b9bfb
Mariano Batistutti <batistutti@gmail.com>
2023-09-12 17:30:53 -0300

    Add project files.

---
+++ PokemonShopping.Domain/ShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonShopping.Domain
+{
+    public class ShoppingCart
+    {
+        public Guid Id { get; set; }
+        public User User { get; set; }
+        public List<Product> Products { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ PokemonShopping.Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonShopping.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<ShoppingCart> ShoppingCarts { get; set; }
+        public List<PurchaseHistory> PurchaseHistory { get; set; }
+    }
+}
