8e5f2c3a15fe01848016ae14f443909746bcc52e
Mariano Batistutti <batistutti@gmail.com>
2023-09-20 15:44:15 -0300

    logout

--- UI/pokemonShopping/src/app/modules/shared/private-layaout/private-layaout.component.ts
+++ UI/pokemonShopping/src/app/modules/shared/private-layaout/private-layaout.component.ts
@@ -3,6 +3,8 @@ import { Store } from '@ngrx/store';
 import { CartState } from '../../cart/+state/cart.reducer';
 import { Observable } from 'rxjs';
 import { selectQuantity } from '../../cart/+state/cart.selector';
+import { AuthState } from '../../+state/auth.reducer';
+import { logout } from '../../+state/auth.actions';
 
 @Component({
   selector: 'app-private-layaout',
@@ -13,13 +15,15 @@ export class PrivateLayaoutComponent implements OnInit {
 
   $quantity: Observable<number>;
 
-  constructor(private cartStore: Store<CartState>) {
+  constructor(private cartStore: Store<CartState>, private authStore: Store<AuthState>) {
     this.$quantity = this.cartStore.select(selectQuantity);
   }
 
-
-
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.authStore.dispatch(logout());
   }
 
 }
